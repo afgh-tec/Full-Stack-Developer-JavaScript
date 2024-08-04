31e4542b9e88ab56094060a419fd3f6142df1d78
afgh <augustogutierrezh@gmail.com>
2024-08-03 21:07:08 -0500

    variables y tipos de datos

---
+++ Curso-de-Fundamentos-de-JavaScript/02-clase-variables.js
@@ -0,0 +1,13 @@
+
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = 'Woody' //No descriptivo
+let cda = 'Woody' //No descriptivo
+let pcAndy = 'Woody' //No descriptivo
+
+// Lo permitido
+let primerTrasteoDeAndy = 'Woody' // Descriptivo
+let urlDelUsuario = 'https://www.google.com' // Descriptivo
+let idDelUsuario = '123456789' // Descriptivo
---
+++ Curso-de-Fundamentos-de-JavaScript/03-tipos-de-datos.js
@@ -0,0 +1,23 @@
+
+// PRIMITIVOS
+let nombre = 'Augusto' // string
+let edad = 25 // number
+let esMayorDeEdad = true // boolean
+let noHayValor = null // null
+let noDefinido = undefined // undefined 
+let simboloUnico = Symbol('único') // symbol
+let numeroGrande = 2n // bigint
+
+// COMPLEJOS
+
+// object
+let carro = {
+  marca: 'Tesla',
+  modelo: 'Model S'
+}
+
+// array
+let frutas = ['manzana', 'banano', 'uvas']
+
+// function
+function saludar () {}
