d7dd4dda2fe2e593a4786c04503496989d097453
afgh <augustogutierrezh@gmail.com>
2024-08-04 11:38:42 -0500

    string, number y operaciones aritmeticas

---
+++ Curso-de-Fundamentos-de-JavaScript/04-string.js
@@ -0,0 +1,17 @@
+// Tipos de datos: STRING
+
+let string1 ="Hola Mundo, comillas dobles" //Comillas ""
+let string2 = 'Hola Mundo, comillas simples' //Comillas ''
+let string3 = `${string1}, concatenando con comillas invertidas` //Comillas ``
+let string4 = string1 + ' ' + string2 //concatenación de string
+
+
+let miFase = 'Me gusta programar con Javascript'
+console.log('Numero total de caracteres: ' + miFase.length)
+console.log('Frase en minúscula: ' + miFase.toLowerCase() )
+console.log('Frase en mayúsculas: ' + miFase.toUpperCase() )
+console.log('Imprime en un rango: ' + miFase.substring(0,18) )
+
+
+
+
