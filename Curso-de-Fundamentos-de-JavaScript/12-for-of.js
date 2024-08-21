5dd44358107b0fdbd06dd8870700395c44196896
afgh <augustogutierrezh@gmail.com>
2024-08-21 12:21:56 -0500

    elimina carpetas repetidas

--- Curso-de-Fundamentos-de-JavaScript/12-for-of.js
+++
@@ -1,8 +0,0 @@
-
-//Solo funciona cuanto tenemos objetos o listas iterables
-let canasta = ["manzana", "pera", "naranja", "uva"];
-//for of array, string[algo]
-//for(variable of objeto)
-for (fruta of canasta) {
-  console.log(fruta);
-}
